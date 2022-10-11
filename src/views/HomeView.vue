e3c604de3304c0b7b661e49a6b52e4854a8394db
sathy77 <18101077@uap-bd.edu>
2022-10-11 23:25:51 +0600

    table

---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+
+    <TableComponent :headers="headersName" :keys="keysName" :data="bdData"></TableComponent>
+  </div>
+</template>
+
+<script>
+import TableComponent from '@/components/TableComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    TableComponent
+  },
+  data(){
+        return {
+            headersName: ['Conutry', 'Division', 'District'],
+            keysName: ['country', 'division', 'district'],
+            bdData: [
+                {
+                    country: 'Bangladesh',
+                    division: 'Dhaka',
+                    district: 'Madaripur'
+                },
+                {
+                    country: 'Bangladesh',
+                    division: 'Sylhet',
+                    district: 'Sunamgang'
+                },
+                {
+                    country: 'Bangladesh',
+                    division: 'Chitagong',
+                    district: 'Bandarban'
+                },
+                {
+                    country: 'Bangladesh',
+                    division: 'Khulna',
+                    district: 'Satkhira'
+                },
+                {
+                    country: 'Bangladesh',
+                    division: 'Mymensing',
+                    district: 'Sherpur'
+                },
+                {
+                    country: 'Bangladesh',
+                    division: 'Dhaka',
+                    district: 'Dhaka'
+                },
+                {
+                    country: 'Bangladesh',
+                    division: 'Barisal',
+                    district: 'Bhola'
+                }
+            ]
+        }
+    }
+
+}
+</script>
